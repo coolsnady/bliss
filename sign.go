ee8ef3afd9bb98e52553ed24ce6de02365b76111
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2018-01-26 22:23:25 +0100

    tentative new random oracle

--- sign.go
+++ sign.go
@@ -28,50 +28,27 @@ func computeC(kappa uint32, u *poly.PolyArray, hash []byte) []uint32 {
 		hash = append(hash, byte(data[i]&0xff))
 		hash = append(hash, byte((data[i]>>8)&0xff))
 	}
-	for tries := 0; tries < 256; tries++ {
-		hash[len(hash)-1]++
-		whash := sha3.Sum512(hash)
-		array := make([]bool, n)
-		if n == 256 {
-			// BLISS_B_0: we need kappa indices of 8 bits
-			i := 0
-			for j := 0; j < int(sampler.SHA_512_DIGEST_LENGTH); j++ {
-				index := whash[j]
-				if !array[index] {
-					indices[i] = uint32(index)
-					array[index] = true
-					i++
-					if i >= int(kappa) {
-						return indices
-					}
-				}
-			}
-		} else {
-			// BLIS_B_1234: we need kappa indices of 9 bits
-			// n should be 512 now
-			extraBits := byte(0)
-			i := 0
-			j := 0
-			for j < int(sampler.SHA_512_DIGEST_LENGTH) {
-				if j&7 == 0 {
-					extraBits = whash[j]
-					j++
-				}
-				index := (uint32(whash[j]) << 1) | uint32(extraBits&1)
-				extraBits >>= 1
-				j++
-				if !array[index] {
-					indices[i] = index
-					array[index] = true
-					i++
-					if i >= int(kappa) {
-						return indices
-					}
-				}
-			}
-		}
-	}
-	return []uint32{}
+        h := sha3.NewShake256()
+        h.Write(hash)
+
+        index := make([]byte, 2)
+        indices_count := 0
+        for indices_count < int(kappa) {
+            h.Read(index)
+            // modulo reduction ok since n is a power of 2 (256 or 512)
+            idx :=  ( int(index[0]) << 8 + int(index[1]) ) % n
+            j := 0
+            for j = 0; j < indices_count; j++ {
+                if indices[j] == uint32(idx) {
+                    break
+                }
+            }
+            if j == indices_count {
+                indices[indices_count] = uint32(idx)
+            }
+            indices_count++
+        }
+        return indices
 }
 
 func greedySc(indices []uint32, s1, s2 *poly.PolyArray) (v1, v2 *poly.PolyArray) {
